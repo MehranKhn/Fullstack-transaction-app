d5df8cd0423188cb9dfc5353daa7a3ef26a77806
Mehran Khan <abdullahabdullah6639@gmail.com>
2025-07-07 20:03:17 +0530

    Configured backend to listen on the deployer-assigned PORT

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 module.exports = {
   jwtSecret: process.env.JWT_SECRET,
-  mongoURI: process.env.MONGO_URI
+  mongoURI: process.env.MONGO_URI,
+  PORT:process.env.PORT || 3000
 };
--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app=express();
 const mainRouter=require('./routes/index')
+const {PORT}=require("./config")
 const cors = require('cors');
 
 app.use(express.json());
@@ -9,7 +10,7 @@ app.use(cors());
 
 app.use('/api/v1',mainRouter)
 
-app.listen(3000,()=>{
+app.listen(PORT,()=>{
     console.log("Server is up and running");
 });
 
